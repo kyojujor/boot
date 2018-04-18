0249c7571c3160f9808be4d49ee44504caed2792
aiming3000 <2415831506@qq.com>
2018-04-18 22:05:19 +0800

    start

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aiming3000
+ * Date: 2018/3/29
+ * Time: 15:29
+ */
+namespace app\admin\controller;
+Class Index
+{
+    public function index(){
+        dump("后台测试");
+    }
+}
